049604648ef915ffdfdf1b891a0e1d2a94a5ffa4
FatmaElMahdi1000 <f69246984@gmail.com>
2025-11-18 15:35:40 +0200

    solving problems!

--- Practice/Alphabet_Pattern.py
+++ Practice/Alphabet_Pattern.py
@@ -1,5 +1,14 @@
-for r in range(1, 8):
-    if r == 1 or r == 7:
-        print("*" * 7)
+#Z shape
+# for r in range(1, 8):
+#     if r == 1 or r == 7:
+#         print("*" * 7)
+#     else:
+#         print(" " * (7 - r) + "*")
+
+#T shape
+for r in range(1, 6):
+    if r == 1:
+        print("*" * 5, end="")
     else:
-        print(" " * (7 - r) + "*")
+        print(" "*2 + "*" + " "*2,  end="")
+    print()
