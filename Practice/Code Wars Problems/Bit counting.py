f2f9eb414cbb36204b3f62e8689af00c0da1f13f
FatmaElMahdi1000 <f69246984@gmail.com>
2025-12-08 20:46:39 +0200

    Solving problems

--- Practice/Code Wars Problems/Bit counting.py
+++ Practice/Code Wars Problems/Bit counting.py
@@ -1,21 +1,24 @@
-binary_repr = ""
-digit_count = 0
-number = 4
-division_int = number / 2
-if number == 0:
-    binary_repr += "1"
-    digit_count += 1
-while number != 0: #1
-    remainder = number % 2
-    number = number // 2
-    if remainder != 0:
+#converting from Decimal to Binary.
+def count_bits(n):
+    binary_repr = ""
+    digit_count = 0
+    if n == 0:
         binary_repr += "1"
-    else:
-        binary_repr += "0"
-    digit_count += 1
+        digit_count += 1
+    while n != 0: #1
+        remainder = n % 2
+        n = n // 2
+        if remainder != 0:
+            binary_repr += "1"
+            digit_count += 1
+        else:
+            binary_repr += "0"
+            digit_count += 1
+    return binary_repr, digit_count
 
-print(binary_repr[::-1])
-print(digit_count)
+n = int(input("Enter a number: "))
+print(count_bits(n))
+
 
 
 
